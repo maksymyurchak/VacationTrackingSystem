3c6ba30247af7d62cd6f839bfdd6e3df8a51cdf0
maksymyurchak <wukarenf@gmail.com>
2017-10-26 20:31:53 +0300

    add middleware

---
+++ VacationTrackingSystem/VacationTrackingSystem/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,33 @@
+﻿using BAL.Managers;
+using DAL;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace VacationTrackingSystem.Providers
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            var userManager = new UserManager(new UnitOfWork());
+            var userDb = userManager.Find(context.UserName, context.Password);
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", userDb.Email));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+
+        }
+    }
+}
